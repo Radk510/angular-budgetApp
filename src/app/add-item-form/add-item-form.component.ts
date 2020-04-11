9bf9386c53daa17ee94d60f7685ec7f06440df68
radk510 <radk510@yandex.ru>
2020-04-11 15:22:19 +0300

    create project

---
+++ src/app/add-item-form/add-item-form.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { BudgetItem } from 'src/shared/models/budget-item.model';
+
+@Component({
+  selector: 'app-add-item-form',
+  templateUrl: './add-item-form.component.html',
+  styleUrls: ['./add-item-form.component.scss']
+})
+export class AddItemFormComponent implements OnInit {
+
+  @Input() item: BudgetItem;
+
+  @Output() onFormSubmit: EventEmitter<BudgetItem> = new EventEmitter<BudgetItem>();
+
+  isNewItem: boolean;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    // if item has a value
+    if(this.item) {
+      this.isNewItem = false;
+    } else {
+      this.isNewItem = true;
+      this.item = new BudgetItem('', null);
+    }
+  }
+
+  onSubmit(form: NgForm){
+    this.onFormSubmit.emit(form.value);
+    form.reset();
+  }
+
+}
---
+++ src/shared/models/budget-item.model.ts
@@ -0,0 +1,12 @@
+export class BudgetItem {
+
+    // description: string;
+
+    // amount: number;
+
+    constructor(public description: string, public amount: number) {
+        // this.description = description;
+        // this.amount = amount;
+    }
+
+}
